e1a4e66d57a00a2c4f3be1d2ad0f55e8f912139b
atagiev <antontagiev12@gmail.com>
2020-12-24 15:06:22 +0300

    upd

--- g.cpp
+++ g.cpp
@@ -7,9 +7,7 @@ int main() {
     string string1;
     cin>>string1;
     int n =count(string1.begin(), string1.end(), 'a');
-    //int x =count(string1.begin(), string1.end(), 'A');
     replace( string1.begin(), string1.end(), 'a', 'b');
-    //replace( string1.begin(), string1.end(), 'A', 'b');
     cout<<string1<<endl;
     cout<<n;
 }
